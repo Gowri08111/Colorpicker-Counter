573a9166f2ceff1c88e40da5fac5a2aab7090385
GOWRI P <69392824+Gowri08111@users.noreply.github.com>
2020-08-30 22:33:26 +0530

    Colorpicker-Counter
    Changing the background color
    Counter

---
+++ script.js
@@ -0,0 +1,34 @@
+function green() {
+
+
+    document.getElementById("main").style.backgroundColor = "green";
+}
+
+
+function red() {
+    document.getElementById("main").style.backgroundColor = "red";
+}
+
+
+function blue() {
+
+    document.getElementById("main").style.backgroundColor = "blue";
+}
+var i = 0;
+
+function add() {
+
+
+    i++;
+    document.getElementById("num").innerHTML = i;
+
+}
+
+function sub() {
+
+    i--;
+    document.getElementById("num").innerHTML = i;
+
+
+
+}
